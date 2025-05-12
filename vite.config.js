0f425f7410a8819856d9ff6a84ba519bfc5c1533
Brozi <jasiubar@gmail.com>
2025-05-12 17:23:26 +0200

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 // vite.config.js
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     base: '/projekt-2/', // needed for GitHub Pages!
     build: {
         outDir: 'dist',
     },
+    plugins: [
+        tailwindcss(),
+    ],
 })
